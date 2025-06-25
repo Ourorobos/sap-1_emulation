cac35b3e1a7b29f9af0f884f193f7cd0dc94b486
Ourorobos <ourorobos.digums@gmail.com>
2025-06-24 20:29:29 -0600

    Added command flags
    added the command flags for the control word

--- hardware_modules/controler.py
+++ hardware_modules/controler.py
@@ -9,10 +9,35 @@ from module import Module
 
 class Controler(Module):
     clock: int
+    tState: int
+    tStateLen: int
+
+    #command flags
+    fClock: bool #clk and inverted clk
+    fClear: bool #clr and inverted clr
+    
+    fCount_next: bool #cp
+    fCount_out: bool #ep
+    
+    fload_mar: bool #inverted lm
+    fWrite_ram: bool #inverted ce
+    
+    fload_inst: bool #inverted li
+    fWrtie_inst: bool #inverted ei
+    
+    fLoad_a: bool #inverted la
+    fWrite_a: bool #inverted ea
+    fSub: bool #su
+    fWrite_alu: bool #eu
+    fLoad_b: bool #inverted lb
+    
+    fload_o: bool #inverted lo
 
     def __init__(self) -> None:
         super.__init__(self)
         self.clock = 0
+        self.tState = 0
+        self.tStateLen = 6
 
     def getClock(self) -> None:
         remain = clock % 2
@@ -22,4 +47,4 @@ class Controler(Module):
             return True
 
     def update_clock(self) -> None:
-        self.clock += 1
+        self.clock += 1    
