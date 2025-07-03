9149576b18f75ac346c4b06821450596e977cbef
Ourorobos <ourorobos.digums@gmail.com>
2025-07-02 20:34:29 -0600

    Finished?
    Redid lots of this one but it should be done aside from some polish

--- hardware_modules/controler.py
+++ hardware_modules/controler.py
@@ -1,62 +1,73 @@
 """
 This is the Controler/Sequencer for the SAP-1.
 So basicly the brain of the brain.
+The Clock is part of the 'layout'.
 """
 
 # This Will need the Instruction Register to be added
-from data_types import Nibble, Word
+from data_types import Nibble, Byte, Command
+from routine import Instruction, NOP, LDA
 from module import Module
 
 class Controler(Module):
 #atributes
-    clock: int
+    value: Byte
+    instruction: Instruction
     tState: int
-    tStateLen: int
-    wControl: Word #This is layed out the same as in the book Lo at the lowest and Cp at the highest
+    tStateLen: int = 6
+    wControl: Command #This is layed out the same as in the book Lo at the lowest and Cp at the highest
+
+    wAddress: Command
+    wIncrement: Command
+    wMemory: Command
 
 #methods
     def __init__(self) -> None:
         super.__init__(self)
-        self.clock = 0
         self.tState = 0
-        self.tStateLen = 6
-        self.wControl.length = 12
         self.wControl.setInt(0)
-    #clock
-    def getClock(self) -> None:
-        remain = clock % 2
-        if (remian == 0):
-            return False
-        else:
-            return True
 
-    def update_clock(self) -> None:
-        self.clock += 1    
+    def getInstruction(self) -> None:
+        temp = self.value.getUpper()
+        #this is just hard coded for now
+        #how long until I fix this?
+        match temp.getInt():
+            
+            case NOP().code.getInt():
+                self.instruction = NOP()
+            case LDA().code.getInt():
+                self.instruction = LDA()
+    
+            #default
+            case _:
+                self.instruction = NOP()
 
-    #states    
-    def address(self) -> None:
-        #undo the last state(the instruction state)
-        self.wControl.setInt(0)
-        #set the program counter out bit
-        self.wControl.toggleBit(11)
-        #set the Load MAR bit
-        self.wControl.toggleBit(10)
+    def getCommand(self) -> Word:
+        #find state
+        match self.tState:
+            case 0:
+                return self.wAddress
+            case 1:
+                return self.wIncrement
+            case 2:
+                return self.wMemory
+            case 3:
+                self.getInstruction()
+                return self.instruction.command[0]
+            case 4:
+                return self.instruction.command[1]
+            case 5:
+                return self.instruction.command[2]
+            case _:
+                self.tState = 0
+                #self.tState = 4
+                #self.instruction = RESET()
+               # return self.instruction.command[0]
 
-    def increment(self) -> None:
-        #undo the last state(the addres state)
-        self.wControl.setInt(0)
-        #set program counter count bit
-        self.wControl.toggleBit(12)
+    def getCommandFlag(self, index: int) -> bool:
+        return self.wControl.getBit(index)
 
-    def memory(self) -> None:
-        #undo the last state(the increment state)
-        self.wControl.setInt(0)
-        #set the RAM out bit
-        self.wControl.toggleBit(9)
-        #set the instruction in bit
-        self.wControl.toggleBit(8)
-
-    def instruction(self) -> None:
-        #undo the last state(the memory state)
-        self.wControl.setInt(0)
-        
+    def update(self) -> None:
+        self.wControl = self.getCommand()
+        self.tState += 1
+        self.tState %= self.tStateLen
